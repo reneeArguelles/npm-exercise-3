bcccc02d598abddae41c15fdf8bee6bc01ea6e42
Mary Renee Arguelles <mary.arguelles@mary.arguelles-CPJFF29RD7>
2022-10-07 14:15:56 +0800

    Add infix-to-postfix converter

---
+++ index.js
@@ -0,0 +1,4 @@
+const { argv } = require('node:process');
+const { parse, toString } = require('@estilles/expression-parser');
+
+console.log(argv.length === 2 ? "Missing infix expression." : toString(parse(argv[2])));
